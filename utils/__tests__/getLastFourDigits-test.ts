cd5f02f33e844e90fa91012b4324237e1f814438
Omar Shatani <omar.shatani@lastminute.com>
2024-08-30 00:33:44 +0200

    added test for getLastFourDigits

---
+++ utils/__tests__/getLastFourDigits-test.ts
@@ -0,0 +1,23 @@
+import { getLastFourDigits } from "@/utils/getLastFourDigits";
+
+describe("getLastFourDigits", () => {
+  describe("when digits less than 4", () => {
+    it("should return the original digits", () => {
+      // When
+      const digits = getLastFourDigits(aThreeDigits);
+      // Then
+      expect(digits).toEqual("123");
+    });
+  });
+  describe("when digits more than 4", () => {
+    it("should return the last 4 digits", () => {
+      // When
+      const digits = getLastFourDigits(aFiveDigits);
+      // Then
+      expect(digits).toEqual("2345");
+    });
+  });
+});
+
+const aThreeDigits = "123";
+const aFiveDigits = "12345";
