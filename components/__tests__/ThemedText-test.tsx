52cb96608b2a340ff760681d8e2988c96e3eab8a
Omar Shatani <omar.shatani@lastminute.com>
2024-08-31 03:47:16 +0200

    added ui tests to ThemedText

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -1,13 +1,77 @@
-import { it, expect } from "@jest/globals";
 import * as React from "react";
-import renderer from "react-test-renderer";
-
 import { ThemedText } from "../ThemedText";
+import { render } from "@testing-library/react-native";
 
-it(`renders correctly`, () => {
-  const tree = renderer
-    .create(<ThemedText>Snapshot test!</ThemedText>)
-    .toJSON();
+describe("ThemedText", () => {
+  it("should render", () => {
+    const screen = render(<ThemedText testID={"Hello"}>Hello</ThemedText>);
 
-  expect(tree).toMatchSnapshot();
+    expect(screen.getByTestId("HelloText")).toBeVisible();
+    expect(screen.getByTestId("HelloText")).toHaveStyle({
+      color: "#11181C",
+      fontSize: 16,
+      lineHeight: 24,
+    });
+  });
+
+  it("should change color if passed by prop", () => {
+    const screen = render(
+      <ThemedText testID={"Hello"} lightColor={"#fff"}>
+        Hello
+      </ThemedText>,
+    );
+
+    expect(screen.getByTestId("HelloText")).toBeVisible();
+    expect(screen.getByTestId("HelloText")).toHaveStyle({
+      color: "#fff",
+      fontSize: 16,
+      lineHeight: 24,
+    });
+  });
+
+  describe("types", () => {
+    const styles = {
+      default: {
+        fontSize: 16,
+        lineHeight: 24,
+      },
+      defaultSemiBold: {
+        fontSize: 16,
+        lineHeight: 24,
+        fontWeight: "600",
+      },
+      title: {
+        fontSize: 32,
+        fontWeight: "bold",
+        lineHeight: 32,
+      },
+      subtitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+      },
+      link: {
+        lineHeight: 30,
+        fontSize: 16,
+        color: "#0a7ea4",
+      },
+    };
+
+    it.each([
+      ["defaultSemiBold", styles.defaultSemiBold],
+      ["title", styles.title],
+      ["subtitle", styles.subtitle],
+      ["link", styles.link],
+    ])(
+      "renders the correct style for %s",
+      (type: any, style: Record<string, string | number>) => {
+        const screen = render(
+          <ThemedText testID={"Hello"} type={type}>
+            Hello
+          </ThemedText>,
+        );
+
+        expect(screen.getByTestId("HelloText")).toHaveStyle(style);
+      },
+    );
+  });
 });
